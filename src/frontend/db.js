3b147d69b5396c76e11a937d11c58601a9233f28
Tony Ivanov <telamohn@pm.me>
2021-10-14 18:11:10 +0200

    Fixed inital values for picoHooks

--- src/frontend/db.js
+++ src/frontend/db.js
@@ -7,9 +7,9 @@ const DB = levelup(leveljs('picochat')) // Open IndexedDB
 export const kernel = new Kernel(DB)
 
 // PicoHook
-export function usePico (store, name, selector) {
+export function usePico (store, name, selector, initialValue) {
   selector = selector || (state => state)
-  const [value, set] = useState()
+  const [value, set] = useState(initialValue)
   useEffect(() => {
     if (!kernel.ready) return
     set(selector(store.state[name]))
@@ -21,9 +21,9 @@ export function usePico (store, name, selector) {
 
 // Helpers hooks for quick register access
 export function useProfile () {
-  return usePico(kernel.store, 'peers', state => state[kernel.pk.toString('hex')])
+  return usePico(kernel.store, 'peers', state => state[kernel.pk.toString('hex')], {})
 }
 
 export function useFriendsList () {
-  return usePico(kernel.store, 'peers', s => Object.values(s).filter(p => p.pk !== kernel.pk))
+  return usePico(kernel.store, 'peers', s => Object.values(s).filter(p => p.pk !== kernel.pk), [])
 }
