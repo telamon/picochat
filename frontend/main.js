4d4e7ed7eb3007a7ee5bd673b19490ff6f1a953f
Tony Ivanov <telamohn@pm.me>
2022-01-12 03:12:34 +0100

    Removed 1.x frontend

--- frontend/main.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './app/App.jsx'
-const element = React.createElement(App, { }, null)
-ReactDOM.render(element, document.getElementById('root'))
